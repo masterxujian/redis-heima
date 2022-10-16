6cd3600030768fff6bc0d076518ab7e4a6c94188
EasyMoneyXj <xj0329zz@163.com>
2022-10-16 20:37:43 +0800

    最基本的配置

--- redis-demo/src/test/java/com/example/redisdemo/RedisDemoApplicationTests.java
+++ redis-demo/src/test/java/com/example/redisdemo/RedisDemoApplicationTests.java
@@ -1,13 +1,27 @@
 package com.example.redisdemo;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.RedisTemplate;
+
+import javax.annotation.Resource;
 
 @SpringBootTest
 class RedisDemoApplicationTests {
 
+
+
+    @Resource
+    private RedisTemplate<String,Object> redisTemplate;
+
     @Test
-    void contextLoads() {
+    void testString() {
+        // 写入一条String数据
+        redisTemplate.opsForValue().set("name", "tiger");
+        // 获取string数据
+        Object name = redisTemplate.opsForValue().get("name");
+        System.out.println("name = " + name);
     }
 
 }
